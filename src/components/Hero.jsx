bfad67855bd2c9bee6aca8e3bc75f938f93edb91
kathir0206 <kathiresanmsc23@gmail.com>
2023-10-25 09:43:09 +0530

    link fix1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,6 @@ const Hero = () => {
             The <span className="text-orange-500">Best</span>
           </h1>
           <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
-            {" "}
             <span className="text-orange-500">Foods</span> Devlivered
           </h1>
         </div>
